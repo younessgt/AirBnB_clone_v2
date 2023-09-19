6c5d93a38c33504c3e740b55154782eecc3f3e96
leila111111 <leila_merzak@outlook.fr>
2023-09-19 20:20:35 +0000

    task9

--- models/place.py
+++ models/place.py
@@ -3,7 +3,7 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, Integer, Float, ForeignKey
 from os import getenv
-
+from sqlalchemy.orm import relationship
 
 class Place(BaseModel, Base):
     """ A place to stay """
