ef7abefcdc9df40af80df4e6c43fd9ad4bf2d538
yonessgt <youness.merzak123@gmail.com>
2023-09-20 18:18:34 +0000

    add db condition

--- models/amenity.py
+++ models/amenity.py
@@ -4,10 +4,13 @@ from models.base_model import BaseModel, Base
 from sqlalchemy.orm import relationship
 from models.place import place_amenity
 from sqlalchemy import Column, String
+from os import getenv
 
 
 class Amenity(BaseModel, Base):
     """creating Amenity class"""
     __tablename__ = "amenities"
-    name = Column(String(128), nullable=False)
-    place_amenities = relationship("Place", secondary=place_amenity)
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        name = Column(String(128), nullable=False)
+        place_amenities = relationship("Place", secondary=place_amenity,
+                                       backref="amenities")
