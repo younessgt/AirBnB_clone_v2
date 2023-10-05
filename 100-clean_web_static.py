53460cccffb69b27092f34ea156aea608299d175
yonessgt <youness.merzak123@gmail.com>
2023-10-05 23:00:51 +0000

    task4

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -1,10 +1,20 @@
 #!/usr/bin/python3
 """ Script that deletes out-of-date archives """
-from fabric.api import local, run, env, cd, lcd
+from fabric.api import local, run, env, cd, runs_once
 
 env.hosts = ['18.235.249.72', '34.239.254.179']
 # env.user = "ubuntu"
 # env.key_filename = '~/.ssh/school'
+
+
+@runs_once
+def clean_local(number):
+    """ cleaning on local """
+    result = local("ls -t version", capture=True)
+    file_list = result.split("\n")
+    file_list = [archive for archive in file_list if archive.strip()]
+    for i in range(int(number), len(file_list)):
+        local(f"rm versions/{file_list[i]}")
 
 
 def do_clean(number=0):
@@ -13,14 +23,7 @@ def do_clean(number=0):
     if int(number) == 0:
         number = 1
 
-    result = local("ls -t", capture=True)
-    file_list = result.split("\n")
-    file_list = [archive for archive in file_list if archive.strip()]
-
-    with lcd("version"):
-        for i in range(int(number), len(file_list)):
-            local(f"rm versions/{file_list[i]}")
-
+    clean_local(number)
     with cd("/data/web_static/releases"):
         result_remote = run("ls -t")
         file_list = result_remote.stdout.split(" ")
