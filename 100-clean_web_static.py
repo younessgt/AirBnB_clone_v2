edd7179c1bc6b2e2e126d53b0052b9ed8dddc451
yonessgt <youness.merzak123@gmail.com>
2023-10-06 17:47:50 +0000

    100-clean_web_static.py

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -19,6 +19,6 @@ def do_clean(number=0):
     for i in range(int(number), len(file_list)):
         local(f"rm versions/{file_list[i]}")
 
-    with cd("/data/web_static/releases/"):
-        run("ls -lt | tail -n +{} | rev | cut -f1 -d" " | rev | \
-        xargs -d '\n' rm".format(1 + int(number)))
+    path = "/data/web_static/releases/*"
+    number = int(number) + 1
+    run("ls -dt {} | tail -n +{} | sudo xargs rm -fr".format(path, number))
