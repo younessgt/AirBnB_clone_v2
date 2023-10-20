c276b6a834360770445e68b2f22253e0d8c2b10b
yonessgt <youness.merzak123@gmail.com>
2023-10-20 15:33:08 +0000

    task 9

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -5,7 +5,6 @@ from flask import Flask
 from models import storage
 from models.state import State
 from flask import render_template
-
 app = Flask(__name__)
 
 
@@ -15,11 +14,11 @@ def close_session(exception):
     storage.close()
 
 
-@app.route("/states_list", strict_slashes=False)
-def states_list():
-    """ display html page with state list """
+@app.route("/cities_by_states", strict_slashes=False)
+def states_cities():
+    """ display html page with states and cities """
     list_state = storage.all(State)
-    return render_template('7-states_list.html', list_state=list_state)
+    return render_template('8-cities_by_states.html', list_state=list_state)
 
 
 if __name__ == '__main__':
